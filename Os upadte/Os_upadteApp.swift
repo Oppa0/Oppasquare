ecaf42e5df754b394eb1d4ba55b2a088f0855c7f
Abhinav <abhinav.sharma@teamcomputers.com>
2023-10-18 12:42:08 +0530

    Initial Commit

---
+++ Os upadte/Os_upadteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Os_upadteApp.swift
+//  Os upadte
+//
+//  Created by Abhinav Sharma on 18/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Os_upadteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
